95fbe4d1047bee13181618bf364134d1f04bed7a
ssong-eu <sangyoon.song@gmail.com>
2018-10-28 12:53:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = 'wat';
+
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = "not " + customerName.toLowerCase();
+}
+
+function overwriteBestCustomer(text){
+  bestCustomer = text;
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "wut";
+}
